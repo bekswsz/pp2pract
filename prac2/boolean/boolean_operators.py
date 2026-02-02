18ec16e3af7a6d142cfd6fd4d56bc79fbcb96faf
BeksultanTemitkhan <temyrhanbeksultan@gmail.com>
2026-02-02 22:16:38 +0500

    exa,ple

---
+++ prac2/boolean/boolean_operators.py
@@ -0,0 +1,27 @@
+#1
+age = 17
+id = True
+print(age >= 21 and id)
+
+
+#2
+a = True
+b = False
+print(a or b)
+
+
+#3
+is_sunny = False
+print(not is_sunny)
+
+
+#4
+ball = 109
+passed_exam = ball >= 110
+print(passed_exam and ball >= 100)
+
+
+#5
+temp = 40
+issummer = True
+print(temp > 30 and issummer)

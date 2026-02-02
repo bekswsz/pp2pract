d21853d6118402048a9ebe312671c520654aab6b
BeksultanTemitkhan <temyrhanbeksultan@gmail.com>
2026-02-02 22:28:15 +0500

    example

---
+++ prac2/if-else/if_statement.py
@@ -0,0 +1,22 @@
+#1
+a = 67
+b = 435
+if b > a:
+  print("b is greater than a")
+
+#2
+number = 17
+if number > 0:
+  print("The number is positive")
+
+#3
+age = 21
+if age >= 18:
+  print("You are an adult")
+  print("You can vote")
+  print("You have full legal rights")
+
+#4
+password = True
+if password:
+  print("Welcome back!")

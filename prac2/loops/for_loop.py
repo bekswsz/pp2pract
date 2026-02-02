5390daab8b3e705a9978a0760a21dbb11340ba10
BeksultanTemitkhan <temyrhanbeksultan@gmail.com>
2026-02-02 23:02:42 +0500

    example

---
+++ prac2/loops/for_loop.py
@@ -0,0 +1,21 @@
+#1
+for i in range(1, 9):
+    print(i)
+
+#2
+a = 0
+for i in range(1, 5):
+    a += i
+print("Sum =", a)
+
+#3
+for fruit in ["apple", "banana", "cherry"]:
+    print(fruit)
+
+#4
+for i in range(1, 11):
+    print(i, i**2)
+
+#5
+for ch in "helloworld":
+    print(ch)

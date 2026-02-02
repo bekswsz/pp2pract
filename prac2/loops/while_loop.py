61d5e40041602700eb34d868705bbf7a02e959bb
BeksultanTemitkhan <temyrhanbeksultan@gmail.com>
2026-02-02 23:14:08 +0500

    example

---
+++ prac2/loops/while_loop.py
@@ -0,0 +1,30 @@
+#1
+i = 3
+while i <= 10:
+    print(i)
+    i += 1
+
+#2
+i = 1
+s = 0
+while i <= 5:
+    s += i
+    i += 1
+print("Sum =", s)
+
+#3
+i = 3
+while i > 0:
+    print(i)
+    i -= 1
+
+#4
+text = ""
+while text != "exit":
+    text = input("Enter command: ")
+
+#5
+i = 2
+while i <= 10:
+    print(i, i**i)
+    i += 1

24b73dfd26ab1dec28c8b2c81f1fc07082b14890
BeksultanTemitkhan <temyrhanbeksultan@gmail.com>
2026-02-02 22:21:36 +0500

    example

---
+++ prac2/if-else/if_else.py
@@ -0,0 +1,39 @@
+#1
+a = 155
+b = 22
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+#2
+number = 15
+
+if number % 2 == 0:
+  print("The number is even")
+else:
+  print("The number is odd")
+
+#3
+username = "Beks"
+
+if len(username) > 0:
+  print(f"Welcome, {username}!")
+else:
+  print("Error: Username cannot be empty")
+
+#4
+number = int(input("Enter a number: "))
+
+if number % 2 == 0:
+    print("Even number")
+else:
+    print("Odd number")
+
+#5
+weather = "sunny"
+
+if weather == "sunny":
+    print("Go for a walk")
+else:
+    print("Stay at home")

d56f90d57848719cdee8c8a0e74654ebcd1becbf
BeksultanTemitkhan <temyrhanbeksultan@gmail.com>
2026-02-02 21:32:50 +0500

    example

---
+++ prac2/boolean/boolean_comparison.py
@@ -0,0 +1,28 @@
+#1
+a = 67
+b = 22
+print(a > b)
+
+
+#2
+x = 5
+y = 5
+print(x == y)
+
+
+#3
+p = 8
+q = 9
+print(p != q)
+
+
+#4
+aa = 17
+bb = aa >= 18
+print(bb)
+
+
+#5
+name1 = "Beksultan"
+name2 = "Bexsultan"
+print(name1 == name2)

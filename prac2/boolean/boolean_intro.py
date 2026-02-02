9163ff92cf65a65e8e0aaaa63700c7131ac33ad2
BeksultanTemitkhan <temyrhanbeksultan@gmail.com>
2026-02-02 22:10:02 +0500

    example

---
+++ prac2/boolean/boolean_intro.py
@@ -0,0 +1,29 @@
+#1
+print(100 > 99)
+print(100 == 99)
+print(100 < 99)
+
+#2
+a = 67
+b = 55
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+#3
+print(bool("Hello"))
+print(bool(67))
+
+#4
+x = "Hello World"
+y = 17
+
+print(bool(x))
+print(bool(y))
+
+#5
+bool("abc")
+bool(123)
+bool(["man", "woman", "you"])
